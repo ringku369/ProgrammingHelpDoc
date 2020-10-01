748f016115ceb0c750e0e607f13fdeb45b300f54
Saim <streloy@gmail.com>
2020-10-01 13:50:10 +0600

    up

---
+++ DB/ORCL/orcl.help.sql
@@ -0,0 +1,73 @@
+TRUNCATE TABLE STUDENTS;
+
+CREATE TABLE STUDENTS (
+
+	id NUMBER (4) NOT NULL,
+	name VARCHAR(128) NULL
+);
+
+
+CREATE SEQUENCE AUTO_INCREMENT_SEQ START WITH 1;
+
+CREATE OR REPLACE TRIGGER STUDENTS_AUTO_INCREMENT 
+BEFORE INSERT ON STUDENTS 
+FOR EACH ROW
+
+BEGIN
+  SELECT AUTO_INCREMENT_SEQ.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+
+
+select * from STUDENTS;
+
+INSERT INTO STUDENTS (NAME) VALUES  ('Saim');
+INSERT INTO STUDENTS (NAME) VALUES  ('Ringku');
+INSERT INTO STUDENTS (NAME) VALUES  ('Jibran');
+INSERT INTO STUDENTS (NAME) VALUES  ('Nipa');
+INSERT INTO STUDENTS (NAME) VALUES  ('Nico');
+
+
+
+#### cmd >  
+#
+# sqlplus 
+# system
+# Oracle123
+# 
+#### sql > conn username/password;
+
+show user;
+select * from tab;
+select username from all_users;
+
+
+DROP USER RINGKU CASCADE;
+
+#CREATE USER books_admin IDENTIFIED BY MyPassword;
+
+CREATE USER RINGKU IDENTIFIED BY Oracle123;
+
+
+GRANT 
+RESOURCE,
+CREATE SESSION, 
+CREATE TABLE, 
+CREATE VIEW, 
+CREATE SEQUENCE, 
+CREATE ANY SEQUENCE, 
+CREATE ANY SYNONYM, 
+CREATE ANY TRIGGER, 
+CREATE PROCEDURE TO RINGKU;
+
+
+#### cmd >
+EXP RINGKU/Oracle123@ORCL FILE=E:\Orcdb\ringku.dmp LOG=E:\Orcdb\ringku.log;
+IMP RINGKU/Oracle123@ORCL FILE=E:\Orcdb\ringku.dmp LOG=E:\Orcdb\ringku.log FULL=Y;
+
+exp system/manager file=emp.dmp log=emp_exp.log full=y
+imp system/manager file=emp.dmp log=emp_imp.log full=y
+
+
+
