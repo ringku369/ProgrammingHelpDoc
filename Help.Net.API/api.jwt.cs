0068ef98181c5f00367cb9e4b7abbce76b55ce6f
ringku369 <ringku369@gmail.com>
2020-10-04 20:38:16 +0600

    up

---
+++ Help.Net.API/api.jwt.cs
@@ -0,0 +1,7 @@
+// https://codeburst.io/jwt-auth-in-asp-net-core-148fb72bed03
+
+// https://www.c-sharpcorner.com/article/jwt-json-web-token-authentication-in-asp-net-core/
+
+// https://code-maze.com/authentication-aspnetcore-jwt-1/
+
+// https://code-maze.com/authentication-aspnetcore-jwt-2/
