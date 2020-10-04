bdd96fbdfa9facc13c9962a4e50161e65a47e670
ringku369 <ringku369@gmail.com>
2020-10-04 20:40:04 +0600

    up

--- DB/MSSQL/Scaffold-DbContext.sql
+++ DB/MSSQL/Scaffold-DbContext.sql
@@ -1,3 +1,14 @@
+### To Know JWT-Athentication 
+
+# https://codeburst.io/jwt-auth-in-asp-net-core-148fb72bed03
+
+# https://www.c-sharpcorner.com/article/jwt-json-web-token-authentication-in-asp-net-core/
+
+# https://code-maze.com/authentication-aspnetcore-jwt-1/
+
+# https://code-maze.com/authentication-aspnetcore-jwt-2/
+
+
 ### To Know Details
 
 # https://github.com/CuriousDrive/BookStores/blob/master/BookStoresWebAPI/BookStoresWebAPI/BookStoresWebAPI/Startup.cs
