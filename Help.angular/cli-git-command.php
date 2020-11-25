3943ee729d46e223196b2cd45246984a483a1719
ringku369 <ringku369@gmail.com>
2020-11-25 19:03:12 +0600

    up

--- Help.angular/cli-git-command.php
+++ Help.angular/cli-git-command.php
@@ -12,6 +12,7 @@ git clone https://github.com/ringku369/ProgrammingHelpDoc.git
 
 git init
 git add README.md
+git add ./
 git commit -m "first commit"
 git remote add origin https://github.com/ringku369/AngularLaravelJWTApp.git
 git push -u origin master
